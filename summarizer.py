69954be3fb7845ec3af42e2e25f9e82174f36d34
rani212002 <96629491+rani212002@users.noreply.github.com>
2026-01-12 17:06:47 +0530

    sa

--- summarizer.py
+++ summarizer.py
@@ -1,16 +1,14 @@
 from transformers import pipeline
 
-# Load summarization model
 summarizer = pipeline(
     "summarization",
     model="facebook/bart-large-cnn"
 )
 
-def summarize_itinerary(itinerary_text):
-    summary = summarizer(
-        itinerary_text,
-        max_length=150,
-        min_length=60,
+def summarize_itinerary(text):
+    return summarizer(
+        text,
+        max_length=160,
+        min_length=80,
         do_sample=False
-    )
-    return summary[0]["summary_text"]
+    )[0]["summary_text"]
