3246e79cdf5e7217b7d1563cc37a0dfe6dc75158
rani212002 <96629491+rani212002@users.noreply.github.com>
2026-01-18 21:20:12 +0530

    ....

--- utils.py
+++
@@ -1,6 +0,0 @@
-def validate_inputs(destination, days):
-    if not destination:
-        return False, "Destination cannot be empty"
-    if days <= 0:
-        return False, "Number of days must be greater than 0"
-    return True, ""
