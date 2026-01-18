038788f0343ec78bdb8677772ddc930e17c9001c
rani212002 <96629491+rani212002@users.noreply.github.com>
2026-01-18 18:40:55 +0530

    aaa

--- itinerary_generator.py
+++ itinerary_generator.py
@@ -2,13 +2,13 @@ from transformers import pipeline
 
 generator = pipeline(
     "text2text-generation",
-    model="google/flan-t5-base"
+    model="google/flan-t5-small"
 )
 
 def generate_itinerary(prompt):
     response = generator(
         prompt,
-        max_length=700,
+        max_length=512,
         do_sample=False
     )
 
