8461e5f08c9b922479b8c425bfac23331f64c761
rani212002 <96629491+rani212002@users.noreply.github.com>
2026-01-19 14:30:30 +0530

    Remove summarization functionality from summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,7 +1,1 @@
-from transformers import pipeline
 
-summarizer = pipeline("summarization")
-
-def summarize_itinerary(itinerary_text):
-    summary = summarizer(itinerary_text, max_length=200, min_length=100, do_sample=False)
-    return summary[0]['summary_text']
